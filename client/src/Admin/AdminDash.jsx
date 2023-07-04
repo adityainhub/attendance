0be6a07e92d8589d4c9483b4ae4fc56e3a641cc7
pr24071-sudo <72143359+pr24071-sudo@users.noreply.github.com>
2023-07-04 22:39:35 +0530

    Update AdminDash.jsx

--- client/src/Admin/AdminDash.jsx
+++ client/src/Admin/AdminDash.jsx
@@ -20,9 +20,13 @@ import {
     TablePagination
 } from '@mui/material';
 
+
+import ModalClose from '@mui/joy/ModalClose';
+import Sheet from '@mui/joy/Sheet';
+
 const AdminDash = () => {
 
-
+    const [copen, setcOpen] = React.useState(false);
     const navigate = useNavigate()
     const [currentPage, setCurrentPage] = useState(1);
     const [passengersCount, setPassengersCount] = useState(0);
@@ -33,6 +37,7 @@ const AdminDash = () => {
 
     const [open, setOpen] = React.useState(false);
     const [userData, setuserData] = useState([])
+    const [role, setRole] = useState([])
     const [data, setData] = useState({});
     const [addmember, setaddmember] = useState({
         projectName: ""
@@ -45,17 +50,31 @@ const AdminDash = () => {
         setaddmember({ ...addmember, [name]: value })
     }
 
+        //roles fetch
+        const getallroles = async () => {
+            try {
+                const res = await axios.get(`http://localhost:9000/getallroles`)
+                setRole(res.data)
+    
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
 
     // fetching data using axios------
-    const callAboutPageone = async ()=>{
+    const callAboutPageone = async () => {
         try {
             const res = await axios.get(`http://localhost:9000/alldata`)
             setuserData(res.data)
-         
+
         } catch (error) {
             console.log(error);
         }
     }
+
+
+
 
 
 
@@ -166,11 +185,11 @@ const AdminDash = () => {
                 <div class="container ">
                     <div class="row" data-aos="fade-up">
                         <div class="col-lg-6  intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-                            <h2>Current Ongoing<br />Projects in <span>NexusNova</span></h2>
+                            <h2>Currently Ongoing<br />Projects in <span>NexusNova</span></h2>
                             <div className=''>
                                 <div className='row'>
                                     <div className='col-sm-3  col-md-3 col-lg-4'>
-                                    {/* <Button startDecorator={<Add />}>Add to cart</Button> */}
+                                        {/* <Button startDecorator={<Add />}>Add to cart</Button> */}
                                         <a onClick={() => setOpen(true)} style={{ textDecoration: "None" }} href="#about" class="btn-get-started scrollto">Add Project</a>
                                     </div>
                                     <div className='col-sm-8  col-md-8 col-lg-4'>
@@ -191,22 +210,17 @@ const AdminDash = () => {
                         <div class="col-lg-6    intro-img order-lg-last order-first" data-aos="zoom-out" data-aos-delay="200">
 
                             <ol class="list-group list-group-numbered mt-5 shadow-lg">
-
-
                                 {
                                     userData.map((item, i) => (
                                         <li class="list-group-item d-flex justify-content-between align-items-start">
                                             <div class="ms-2 me-auto">
-                                                <NavLink to={`/contracter/${item.project_id}`} className="text-decoration-none"><div class="fw-bold">{item.projectName}</div></NavLink>
-                                                Content for list item
-
+                                                <NavLink to = {`/roles/${item.project_id}/${item.projectName}`}  onClick={() => setcOpen(true)} className="text-decoration-none"><div class="fw-bold">{item.projectName}</div></NavLink>
                                             </div>
-                                            <i class="bi bi-at"></i>
                                         </li>
 
 
                                     ))}
-                            
+
                             </ol>
                         </div>
                     </div>
@@ -216,7 +230,7 @@ const AdminDash = () => {
 
 
 
-
+            {/* to={`/contractor/${item.project_id}`} */}
 
 
 
@@ -285,6 +299,63 @@ const AdminDash = () => {
 
 
 
+
+            <Modal
+                aria-labelledby="modal-title"
+                aria-describedby="modal-desc"
+                open={copen}
+                onClose={() => setcOpen(false)}
+                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+            >
+                <Sheet
+                    variant="outlined"
+                    sx={{
+                        maxWidth: 500,
+                        borderRadius: 'md',
+                        p: 3,
+                        boxShadow: 'lg',
+                    }}
+                >
+                    <ModalClose
+                        variant="outlined"
+                        sx={{
+                            top: 'calc(-1/4 * var(--IconButton-size))',
+                            right: 'calc(-1/4 * var(--IconButton-size))',
+                            boxShadow: '0 2px 12px 0 rgba(0 0 0 / 0.2)',
+                            borderRadius: '50%',
+                            bgcolor: 'background.body',
+                        }}
+                    />
+                    <Typography
+                        component="h2"
+                        id="modal-title"
+                        level="h4"
+                        textColor="inherit"
+                        fontWeight="lg"
+                        mb={1}
+                    >
+                        The Roles Tagged to a project
+                    </Typography>
+                    <Typography id="modal-desc" textColor="text.tertiary">
+                        <div class="col-lg-12    intro-img order-lg-last order-first" data-aos="zoom-out" data-aos-delay="200">
+
+                            <ol class="list-group list-group-numbered mt-5 shadow-lg">
+                                {
+                                    userData.map((item, i) => (
+                                        <li class="list-group-item d-flex justify-content-between align-items-start">
+                                            <div class="ms-2 me-auto">
+                                                <NavLink  className="text-decoration-none"><div class="fw-bold">{item.projectName}</div></NavLink>
+                                            </div>
+                                        </li>
+
+
+                                    ))}
+
+                            </ol>
+                        </div>
+                    </Typography>
+                </Sheet>
+            </Modal>
 
         </>
     )
