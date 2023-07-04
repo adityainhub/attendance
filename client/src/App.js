07d07a0e5c557819d8f5cbc7bd70bcea9d9e6f15
pr24071-sudo <72143359+pr24071-sudo@users.noreply.github.com>
2023-07-04 23:01:49 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
 import Home from "./Admin/AdminLogin"
 import AdminDash from "./Admin/AdminDash";
-import Contracter from "./Admin/Contractor";
+import Contractor from "./Admin/Contractor";
 import AddMember from "./Admin/AddMember";
 import About from "./Admin/About";
 import Footer from "./Admin/Footer";
+import Roles from "./Admin/Roles";
 
 function App() {
  // const isBackgroundRed=false;
@@ -16,10 +17,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/admindash" element={<AdminDash />} />
-          <Route path='/contracter/:id' element={<Contracter />} />
+          <Route path='/contractor/:project_id/:role_id' element={<Contractor />} />
           <Route path='addmember' element={<AddMember />} />
           <Route path='/about' element={<About />} />
           <Route path='/roles/:id/:projectName' element={<Roles />} />
+          
         </Routes>
         
       
